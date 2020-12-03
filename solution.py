f69be6948ac5f02d75fe01631a9637de7a5eaeba
florianmoss <me@florianmoss.eu>
2020-12-03 20:56:50 +0000

    day3

--- solution.py
+++ solution.py
@@ -4,18 +4,18 @@
 
 import csv
 
-# allLines = []
+allLines = []
 
-# with open('day1.csv', newline='') as data:
-#     reader = csv.DictReader(data)
-#     for row in reader:
-#         allLines.append(int(row['data'].strip()))
-# allLines.sort()
+with open('day1.csv', newline='') as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        allLines.append(int(row['data'].strip()))
+allLines.sort()
 
-# for i in range(10):
-#     for j in range(len(allLines)):
-#         if (allLines[i] + allLines[j]) == 2020:
-#             print(allLines[i] * allLines[j])
+for i in range(10):
+    for j in range(len(allLines)):
+        if (allLines[i] + allLines[j]) == 2020:
+            print("Day 1: {0}".format(allLines[i] * allLines[j]))
 
 
 
@@ -23,19 +23,19 @@ import csv
 # Day 2
 # -------
 
-# allLines = []
-# count = 0
+allLines = []
+count = 0
 
-# with open('day2.csv', newline='') as data:
-#     reader = csv.DictReader(data)
-#     for row in reader:
-#         allLines.append(row['data'].strip().replace('-', ' ').replace(':', ' ').split(' '))
+with open('day2.csv', newline='') as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        allLines.append(row['data'].strip().replace('-', ' ').replace(':', ' ').split(' '))
 
-# for line in allLines:
-#     if int(line[0]) <= line[4].replace(line[2], '1').count('1') <= int(line[1]):
-#         count+=1
+for line in allLines:
+    if int(line[0]) <= line[4].replace(line[2], '1').count('1') <= int(line[1]):
+        count+=1
 
-# print(count)
+print("Day 2: {0}".format(count))
 
 
 # -------
@@ -55,7 +55,7 @@ count = 0
 
 for x in arr:
     if x[pos]=='#':
-        count_2 += 1
+        count += 1
    
     if pos<28:
         pos += 3
@@ -66,5 +66,5 @@ for x in arr:
     else:
         pos = 2
 
-print(count)
+print("Day 3: {0}".format(count))
 
